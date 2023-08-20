9671b7a99fa3483d4b0cbaff07319082a6a972ce
hyakki1327 <hyakki1327@gmail.com>
2023-08-20 10:02:31 +0500

    Fix 1.0

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ static string GetReadableTime(int seconds)
 
     if (seconds > 359999)
         return "Error";
+    else if (seconds < 0)
+        return "Error";
     else
     {
         SS = (seconds % 60).ToString("d2");
-        MM = ((seconds / 60) % 60).ToString("d2");
-        HH = ((seconds / 60) / 60).ToString("d2");
+        MM = (seconds / 60 % 60).ToString("d2");
+        HH = (seconds / 3600).ToString("d2");
     }
 
     ReadableTime = HH + ":" + MM + ":" + SS;
