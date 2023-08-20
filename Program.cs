38eb0ac708f0ef7bb09c2af0edbf8077afac2eb6
Hyakki1327 <Hyakki1327@DESKTOP-RMB5D9S>
2023-08-20 09:54:48 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿var Time = Convert.ToInt32(Console.ReadLine());
+
+static string GetReadableTime(int seconds)
+{
+    string ReadableTime, HH, MM, SS = "00";
+
+    if (seconds > 359999)
+        return "Error";
+    else
+    {
+        SS = (seconds % 60).ToString("d2");
+        MM = ((seconds / 60) % 60).ToString("d2");
+        HH = ((seconds / 60) / 60).ToString("d2");
+    }
+
+    ReadableTime = HH + ":" + MM + ":" + SS;
+
+    return (ReadableTime);
+}
+
+Console.WriteLine(GetReadableTime(Time));
